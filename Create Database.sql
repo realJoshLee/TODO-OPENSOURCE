1ec3167609e87373663be16adf27fcdf41bea18b
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2020-03-23 09:14:17 -0400

    Add files via upload

---
+++ Create Database.sql
@@ -0,0 +1,23 @@
+-- Dumping structure for table webapps.tasks
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL DEFAULT 'No value entered',
+  `user` varchar(50) DEFAULT NULL,
+  `folder` varchar(50) NOT NULL DEFAULT 'inbox',
+  `done` tinyint(4) NOT NULL DEFAULT 0,
+  `created` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table webapps.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `donor` tinyint(4) DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`username`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COMMENT='Users for all web apps.\r\n';
