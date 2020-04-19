d8350e4ea16729a3fa94c8c2b4864bb9bbebd66d
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2020-04-19 07:44:28 -0400

    Finalized some things.

--- Create Database.sql
+++ Create Database.sql
@@ -1,13 +1,26 @@
--- Dumping structure for table webapps.tasks
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL DEFAULT 'No value entered',
-  `user` varchar(50) DEFAULT NULL,
-  `folder` varchar(50) NOT NULL DEFAULT 'inbox',
-  `done` tinyint(4) NOT NULL DEFAULT 0,
-  `created` datetime DEFAULT current_timestamp(),
+-- Dumping structure for table webapps.todofolders
+CREATE TABLE IF NOT EXISTS `todofolders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `foldername` varchar(50) NOT NULL DEFAULT '0',
+  `user` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table webapps.todotasks
+CREATE TABLE IF NOT EXISTS `todotasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(5000) DEFAULT NULL,
+  `user` varchar(50) DEFAULT NULL,
+  `folder` varchar(50) DEFAULT NULL,
+  `day` varchar(50) DEFAULT NULL,
+  `special` varchar(50) NOT NULL DEFAULT 'none',
+  `priority` varchar(50) NOT NULL DEFAULT '3',
+  `done` tinyint(4) NOT NULL DEFAULT 0,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
@@ -16,10 +29,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(256) DEFAULT NULL,
+  `recovery` varchar(50) NOT NULL DEFAULT '0',
   `donor` tinyint(1) NOT NULL DEFAULT 0,
+  `theme` varchar(50) NOT NULL DEFAULT 'light',
   `verified` tinyint(1) NOT NULL DEFAULT 0,
   `admin` tinyint(1) NOT NULL DEFAULT 0,
   `created` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`username`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COMMENT='Users for all web apps.\r\n';
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COMMENT='Users for all web apps.\r\n';
