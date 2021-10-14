43b73e8596c931f6841d8567a40940c737418ee2
Josh Lee <38728523+realJoshLee@users.noreply.github.com>
2021-10-13 20:35:09 -0400

    Deleted old files.
    Deleted old files for this version update.

--- app/__test__/json/multiple-json-obj.js
+++
@@ -1,8 +0,0 @@
-// origional
-a = [{"id": "10", "item": "fridge"}, {"id": "11", "item": "stove"}]
-a.forEach(e => console.log(e));
-
-// new
-jsondata.forEach((e) => {
-  $(`.${e.scheduledate}`).append(e.scheduledate + '<br>');
-});
--- app/dynamic/page/quick-add.php
+++
@@ -1,8 +0,0 @@
-<div class="content">
-  <div class="day-container">
-    <h2><span class="day">Quick Add</span></h2>
-    <form class="task-sub-form" data-id="quickadd" id="form-quickadd">
-      <input type="text" name="task" placeholder="Add a task" class="form-control" id="input-quickadd">
-    </form>
-  </div>
-</div>
